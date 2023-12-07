942d4322fd1f9f16be14386fff005467bf378918
跨性别 <github@lgbt.sh>
2023-12-07 01:47:03 +0100

    [/] Test

--- get.js
+++ get.js
@@ -5,7 +5,6 @@ const {logSuccess, nonascii, DNS_ZONE_ID, logErrorAndExit, ipv4, ipv6, hostname}
 /**
  * CONSTANTS
  */
-const subdomain = array[1][1]
 
 const cf = flare({
   token: process.env.CF_TOKEN
@@ -28,34 +27,34 @@ if (
         array[0][0] === "" &&
         array[1].length === 2 &&
         array[1][0] === "Subdomain Name" &&
-        !subdomain.includes(" ") &&
-        !nonascii.test(subdomain) &&
-        subdomain !== ".lgbt.sh" &&
-        subdomain.endsWith(".lgbt.sh")
+        !array[1][1].includes(" ") &&
+        !nonascii.test(array[1][1]) &&
+        array[1][1] !== ".lgbt.sh" &&
+        array[1][1].endsWith(".lgbt.sh")
     )
 ) {
-    return console.log("not planned|The domain you entered is invalid!|" + subdomain)
+    return console.log("not planned|The domain you entered is invalid!|" + array[1][1])
 }
 
 
 
-if (hostname.test(subdomain)) type = "CNAME"
-if (ipv4.test(subdomain)) type = "A"
-if (ipv6.test(subdomain)) type = "AAAA"
-if (type === "hostname" && !subdomain.includes(".")) type = "invalid"
+if (hostname.test(array[1][1])) type = "CNAME"
+if (ipv4.test(array[1][1])) type = "A"
+if (ipv6.test(array[1][1])) type = "AAAA"
+if (type === "hostname" && !array[1][1].includes(".")) type = "invalid"
 if (type === "invalid") {
-    return logErrorAndExit('The record destination you entered is invalid!', subdomain)
+    return logErrorAndExit('The record destination you entered is invalid!', array[1][1])
 }
 
 cf.dnsRecords.browse(DNS_ZONE_ID)
   .then((dnsRecords) => {
     const availabilityFilter = dnsRecords.result.filter((record) => {
-        return record.name === subdomain
+        return record.name === array[1][1]
     })
 
     if (availabilityFilter[0]) {
-        return logSuccess(`Here's the domain info:\\nRecord Type: ${availabilityFilter[0].type}\\nRecord Content: ${availabilityFilter[0].content}\\nDomain Owner: ${availabilityFilter[0].comment}\\nRegistered On: ${availabilityFilter[0].created_on}\\nLast Modified: ${availabilityFilter[0].modified_on}`, subdomain)
+        return logSuccess(`Here's the domain info:\\nRecord Type: ${availabilityFilter[0].type}\\nRecord Content: ${availabilityFilter[0].content}\\nDomain Owner: ${availabilityFilter[0].comment}\\nRegistered On: ${availabilityFilter[0].created_on}\\nLast Modified: ${availabilityFilter[0].modified_on}`, array[1][1])
     }
-        return logSuccess(`Domain not found, available for registering.`, subdomain)
+        return logSuccess(`Domain not found, available for registering.`, array[1][1])
 
 })
